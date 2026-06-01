9c0e796f7a629026638ec7d8df15710c9e6e6ea8
Lawtro <98205608+Lawtro37@users.noreply.github.com>
2026-06-01 10:36:25 +1000

    resize launcher to fit advanced options and add new flags

--- launcher/preload.js
+++ launcher/preload.js
@@ -4,4 +4,5 @@ contextBridge.exposeInMainWorld("launcherApi", {
     browseConfig: () => ipcRenderer.invoke("launcher:browseConfig"),
     toggle: (payload) => ipcRenderer.invoke("launcher:toggle", payload),
     onStatus: (handler) => ipcRenderer.on("launcher:status", (_e, p) => handler(p)),
+    openAdvanced: (open) => ipcRenderer.invoke("launcher:openAdvanced", open),
 });
